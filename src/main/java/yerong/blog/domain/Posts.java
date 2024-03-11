0ee3d2d8d146cc01617a4503778b573538f88488
nyeroni <nir2y@naver.com>
2024-03-12 01:29:29 +0900

    Feat: Posts(게시글) 엔티티 생성

---
+++ src/main/java/yerong/blog/domain/Posts.java
@@ -0,0 +1,29 @@
+package yerong.blog.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Posts {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "posts_id", updatable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Builder
+    public Posts(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
+}
