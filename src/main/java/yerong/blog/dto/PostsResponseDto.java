769841da28bf3e54f133e3a5face7e662330e4bf
nyeroni <nir2y@naver.com>
2024-03-12 10:12:50 +0900

    Feat: Post 응답 Dto 수정

--- src/main/java/yerong/blog/dto/PostsResponseDto.java
+++ src/main/java/yerong/blog/dto/PostsResponseDto.java
@@ -2,10 +2,16 @@ package yerong.blog.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import yerong.blog.domain.Posts;
 
 @Getter
 @AllArgsConstructor
 public class PostsResponseDto {
     private final String title;
     private final String content;
+
+    public PostsResponseDto(Posts posts){
+        this.title = posts.getTitle();
+        this.content = posts.getContent();
+    }
 }
