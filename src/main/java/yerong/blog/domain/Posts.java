03e131d7dc70ce2e8f12e1d42643f470907c9ce8
nyeroni <nir2y@naver.com>
2024-03-12 13:33:34 +0900

    Feat: 수정 메서드 추가

--- src/main/java/yerong/blog/domain/Posts.java
+++ src/main/java/yerong/blog/domain/Posts.java
@@ -26,4 +26,9 @@ public class Posts {
         this.title = title;
         this.content = content;
     }
+
+    public void update(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
 }
