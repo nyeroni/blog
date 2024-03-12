44df3fd8283e67fb6c19fd7dbd3ec00a3d2b9cd5
nyeroni <nir2y@naver.com>
2024-03-12 12:43:22 +0900

    Feat: Post 전체 조회 컨트롤러 구현

--- src/main/java/yerong/blog/controller/PostsApiController.java
+++ src/main/java/yerong/blog/controller/PostsApiController.java
@@ -3,12 +3,17 @@ package yerong.blog.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import yerong.blog.domain.Posts;
 import yerong.blog.dto.PostsRequestDto;
+import yerong.blog.dto.PostsResponseDto;
 import yerong.blog.service.PostsService;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,6 +26,16 @@ public class PostsApiController {
         Posts savedPost = postsService.save(dto);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(savedPost);
+    }
 
+    @GetMapping("/api/posts")
+    public ResponseEntity<?> findAllPosts(){
+
+        List<PostsResponseDto> postsResponseDtos = postsService.findAll()
+                .stream()
+                .map(PostsResponseDto::new)
+                .toList();
+
+        return ResponseEntity.ok().body(postsResponseDtos);
     }
 }
