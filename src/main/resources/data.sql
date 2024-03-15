d204a111843e1db2444866078dbd19257a9eb883
nyeroni <nir2y@naver.com>
2024-03-15 23:01:08 +0900

    Feat: 오타 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO posts (title, content) VALUES ('저목1', '내용1')
-INSERT INTO posts (title, content) VALUES ('저목2', '내용2')
-INSERT INTO posts (title, content) VALUES ('저목3', '내용3')
+INSERT INTO posts (title, content) VALUES ('제목1', '내용1')
+INSERT INTO posts (title, content) VALUES ('제목2', '내용2')
+INSERT INTO posts (title, content) VALUES ('제목3', '내용3')
