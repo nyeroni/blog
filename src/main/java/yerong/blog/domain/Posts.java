a8348ea9aba74127cf82e6b8f56169a52e6c5162
nyeroni <nir2y@naver.com>
2024-03-15 23:05:12 +0900

    Feat: BaseTimeEntity 생성 및 적용

--- src/main/java/yerong/blog/domain/Posts.java
+++ src/main/java/yerong/blog/domain/Posts.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Posts {
+public class Posts extends BaseTimeEntity{
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "posts_id", updatable = false)
